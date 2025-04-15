1dbca4745557644d9d07bf10bc34841e3f894b54
Duckbutr23 <duckbutr23@gmail.com>
2025-04-15 09:29:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  const backgrounds = ["bg1.jpg", "bg2.jpg"];
-  const chosen = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-  document.body.style.backgroundImage = `url('${chosen}')`;
+  const backgrounds = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
+  const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  document.body.style.backgroundImage = `url(${randomBg})`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundAttachment = "fixed";
 });
